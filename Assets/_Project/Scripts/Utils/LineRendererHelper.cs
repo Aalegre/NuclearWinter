73b71c136405b05b1c6b6f7221d3d8a17c6039a4
Alberto Alegre <alberto.alegre@sirnext.com>
2020-10-23 13:40:35 +0200

    Cosas de animaciones

---
+++ Assets/_Project/Scripts/Utils/LineRendererHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(LineRenderer))]
+public class LineRendererHelper : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public Transform[] positions;
+    int lastPositionsCount = -1;
+    void Awake()
+    {
+        if (!lineRenderer)
+            lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(positions.Length != lastPositionsCount)
+        {
+            lastPositionsCount = positions.Length;
+            lineRenderer.positionCount = lastPositionsCount;
+        }
+        for (int i = 0; i < positions.Length; i++)
+        {
+            try
+            {
+                lineRenderer.SetPosition(i, positions[i].position);
+            }
+            catch { }
+        }
+    }
+}
